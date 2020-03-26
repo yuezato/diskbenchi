75dce7705f3e170a435bb5ae7f5f8ae2357caabe
yuezato <yuuya_uezato@dwango.co.jp>
2020-03-27 03:39:04 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 extern crate libc;
-extern crate rand;
 extern crate structopt;
 
 use libc::c_void;
-use rand::{thread_rng, Rng};
 use std::fs::OpenOptions;
 use std::io::{Seek, SeekFrom, Write};
 use std::os::unix::fs::OpenOptionsExt;
 use std::path::PathBuf;
 use std::ptr;
-use std::time::{Duration, Instant};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -26,17 +23,16 @@ struct Opt {
 
     #[structopt(long)]
     offset: Option<u64>,
+
+    #[structopt(long)]
+    hugepool: bool,
 }
 
 #[derive(Debug)]
 pub struct HugePool {
     pool: *mut libc::c_void,
-    aligned: *mut libc::c_void,
     len: usize,
 }
-
-unsafe impl Sync for HugePool {}
-unsafe impl Send for HugePool {}
 
 impl Drop for HugePool {
     fn drop(&mut self) {
@@ -46,23 +42,19 @@ impl Drop for HugePool {
 
 impl HugePool {
     pub fn to_slice(&self, len: usize) -> &[u8] {
-        unsafe { std::slice::from_raw_parts(self.aligned as *const u8, len) }
+        assert!(len <= self.len);
+        unsafe { std::slice::from_raw_parts(self.pool as *const u8, len) }
     }
 
     pub fn to_mut_slice(&self, len: usize) -> &mut [u8] {
-        unsafe { std::slice::from_raw_parts_mut(self.aligned as *mut u8, len) }
-    }
-
-    pub fn as_mutref(&self) -> *mut u8 {
-        self.aligned as *mut u8
+        assert!(len <= self.len);
+        unsafe { std::slice::from_raw_parts_mut(self.pool as *mut u8, len) }
     }
 
     pub fn new(len: usize) -> Option<HugePool> {
         if len % 4096 != 0 {
             return None;
         }
-        let len = len + 4096;
-
         unsafe {
             let ptr = libc::mmap(
                 ptr::null_mut(),
@@ -76,19 +68,50 @@ impl HugePool {
             if ptr == libc::MAP_FAILED {
                 None
             } else if ptr as usize % 4096 == 0 {
-                dbg!("Good!");
-                Some(HugePool {
-                    pool: ptr,
-                    aligned: ptr,
-                    len,
-                })
+                Some(HugePool { pool: ptr, len })
             } else {
-                dbg!("So-so");
-                Some(HugePool {
-                    pool: ptr,
-                    aligned: (4096 * ((ptr as usize + 4095) / 4096)) as *mut c_void,
-                    len,
-                })
+                // failed
+                libc::munmap(ptr, len);
+                None
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Pool {
+    pool: *mut libc::c_void,
+    len: usize,
+}
+
+impl Drop for Pool {
+    fn drop(&mut self) {
+        unsafe { libc::free(self.pool) };
+    }
+}
+
+impl Pool {
+    pub fn to_slice(&self, len: usize) -> &[u8] {
+        assert!(len <= self.len);
+        unsafe { std::slice::from_raw_parts(self.pool as *const u8, len) }
+    }
+
+    pub fn to_mut_slice(&self, len: usize) -> &mut [u8] {
+        assert!(len <= self.len);
+        unsafe { std::slice::from_raw_parts_mut(self.pool as *mut u8, len) }
+    }
+
+    pub fn new(len: usize) -> Option<Pool> {
+        if len % 4096 != 0 {
+            return None;
+        }
+        unsafe {
+            let mut ptr: *mut c_void = ptr::null_mut();
+            if libc::posix_memalign(&mut ptr, 4096, len) != 0 {
+                // failed
+                None
+            } else {
+                Some(Pool { pool: ptr, len })
             }
         }
     }
@@ -97,30 +120,32 @@ impl HugePool {
 fn main() {
     let opt = Opt::from_args();
     let offset = opt.offset.unwrap_or(0);
+    let hugepool = opt.hugepool;
 
     println!("{:#?}", opt);
 
+    assert!(opt.bs % 4096 == 0);
+
     let mut file = OpenOptions::new()
-        .create(true)
+        .create(true) // create if the designated file doesn't exist
         .write(true)
-        .custom_flags(libc::O_DIRECT)
+        .custom_flags(libc::O_DIRECT) // open with O_DIRECT
         .open(opt.of)
         .expect("Can't open");
 
     file.seek(SeekFrom::Start(offset)).expect("Can't seek");
-    
-    let huge_pool = HugePool::new(10 * 1024 * 1024).expect("Can't make pool");
-    
-    let mut total_time1 = Duration::new(0, 0);
-    let total_time2 = Instant::now();
-    for _ in 0..opt.count {
-        thread_rng().fill(huge_pool.to_mut_slice(opt.bs));
 
-        let now = Instant::now();
-        file.write_all(huge_pool.to_slice(opt.bs)).expect("Write failed");
-        total_time1 += now.elapsed();
+    if !hugepool {
+        let pool = Pool::new(opt.bs).expect("Can't make pool");
+        for _ in 0..opt.count {
+            file.write_all(pool.to_slice(opt.bs)).expect("Write failed");
+        }
+    } else {
+        let huge_pool = HugePool::new(10 * 1024 * 1024)
+            .expect("Can't make huge pool: check /proc/sys/vm/nr_hugepages");
+        for _ in 0..opt.count {
+            file.write_all(huge_pool.to_slice(opt.bs))
+                .expect("Write failed");
+        }
     }
-
-    dbg!(total_time1);
-    dbg!(total_time2.elapsed());
 }
